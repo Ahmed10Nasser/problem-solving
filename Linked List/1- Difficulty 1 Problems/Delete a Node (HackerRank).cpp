4755f52f2b05fe9cbb87a9922495131fbe968099
Ahmed Nasser Zaki <anm1611999@gmail.com>
2023-04-01 21:10:52 +0200

    Create Delete a Node (HackerRank).cpp

---
+++ Linked List/1- Difficulty 1 Problems/Delete a Node (HackerRank).cpp
@@ -0,0 +1,18 @@
+// Problem Link : https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problemv
+
+SinglyLinkedListNode* deleteNode(SinglyLinkedListNode* llist, int position) {
+    if(llist->next==NULL) return llist=NULL;
+    if(position==0){
+        llist=llist->next;
+        return llist;
+    }
+    SinglyLinkedListNode* node=llist;
+    int index=-1;
+    while(node!=NULL){
+        index++;
+        if(index==position-1) break;
+        node=node->next;
+    }
+    node->next=node->next->next;
+    return llist;
+}
